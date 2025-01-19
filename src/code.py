304f6753feebe22bc817d8e787b4630645b5db8a
ryuyuryuyu <nozaru.bosuzaru.hekokizaru@gmail.com>
2025-01-20 00:11:29 +0900

    first commit

---
+++ src/code.py
@@ -0,0 +1,19 @@
+
+def sum_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    return a / b
+
+def power_numbers(a, b):
+    return a ** b
+
+def root_numbers(a, b):
+    return a ** (1/b)
+
---
+++ tests/test_code.py
@@ -0,0 +1,5 @@
+from src import code
+
+def test_sum_numbers():
+    result = code.sum_numbers(2, 3)
+    assert result == 5
